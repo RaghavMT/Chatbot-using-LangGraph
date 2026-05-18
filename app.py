70f8e07adb9bc563f82dd4a1dcc3b23bf8822757
RaghavMT <raghav.mt.8@gmail.com>
2026-05-18 18:24:49 +0530

    adding frontend

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from chatbot import chatbot
+from langchain_core.messages import HumanMessage
+
+#we are using persistence so here we define our thread
+CONFIG = {'configurable' : {'thread_id' : 'thread-1'}}
+
+#st.session is basically a dictoinory that saves previously run data
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+#loding the entire conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    #firstly add the message to message_history
+    st.session_state['message_history'].append({'role' : 'user', 'content' : user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    #now we fetch it from the llm
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+
+    ai_message = response['message'][-1].content
+
+    #first add the ai_message into message_history
+    st.session_state['message_history'].append({'role' : 'assistant', 'content' : ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
