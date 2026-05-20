cd1da37018d2f48eb4da5a42a4fb2069fa7bbd81
RaghavMT <raghav.mt.8@gmail.com>
2026-05-20 16:27:17 +0530

    trying to implement streaming

--- app.py
+++ app.py
@@ -24,11 +24,14 @@ if user_input:
         st.text(user_input)
 
     #now we fetch it from the llm
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+#    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+#
+#    ai_message = response['messages'][-1].content
+#
+#    #first add the ai_message into message_history
+#    st.session_state['message_history'].append({'role' : 'assistant', 'content' : ai_message})
+#    with st.chat_message('assistant'):
+#        st.text(ai_message)
 
-    ai_message = response['messages'][-1].content
-
-    #first add the ai_message into message_history
-    st.session_state['message_history'].append({'role' : 'assistant', 'content' : ai_message})
-    with st.chat_message('assistant'):
-        st.text(ai_message)
+    #we will now use streaming in python to smothen out our chatbot
+    
