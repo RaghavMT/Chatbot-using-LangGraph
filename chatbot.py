db19b24e3fceeaf36672701688be6ebd1cd4c970
RaghavMT <raghav.mt.8@gmail.com>
2026-05-30 22:40:19 +0530

    tools

--- chatbot.py
+++ chatbot.py
@@ -107,7 +107,7 @@ graph.add_node('tools', tool_node)
 #adding edges
 graph.add_edge(START, 'chat_node')
 graph.add_conditional_edges("chat_node", tools_condition)
-graph.add_edge('tools', END)
+graph.add_edge('tools', 'chat_node')
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
